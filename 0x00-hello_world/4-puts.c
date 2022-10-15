b9d9d8f8649bec60a6f9b1cfeea1213023bf3f6f
Sithi <sithimbk@gmail.com>
2022-10-15 16:20:01 +0200

    print text using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
-int main(void) /*Prints to outpuut using puts*/
+/*Prints to outpuut using puts*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
