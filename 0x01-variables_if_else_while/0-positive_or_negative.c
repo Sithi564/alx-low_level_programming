cffc0311a262af8fd21d4b74ed7b3e88cc43007f
Sithi <sithimbk@gmail.com>
2022-10-17 23:12:48 +0200

    Last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    if (n > 0)
     {
         printf("%d is positive\n", n);
     } else if (n == 0)
@@ -21,5 +21,5 @@ int main(void)
     {
         printf("%d is negative\n", n);
     }
-	return (0);
+    return (0);
 }
