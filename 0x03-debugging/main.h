ec05949c37f4137028fb2afde2e6fd2dddbd9ef1
Sithi <sithimbk@gmail.com>
2022-10-19 15:47:36 +0200

    lets see

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,11 @@
-#include <stdio.h>
+#if !defined(main.h)
+#define main.h
+
 /**
  * Positive_or_negative-tests to see if integer value is positive or negative
  * returns number sign
  */
 
 void positive_or_negative(int);
+
+#endif  /* !defined(alx_low_level_ptogramming... 0x02-debugging) */
