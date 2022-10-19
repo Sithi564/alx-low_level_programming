e6b9980c72e04d61c36cbe0249b9488e5382e33d
Sithi <sithimbk@gmail.com>
2022-10-19 14:52:16 +0200

    test

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,7 @@
+#include <stdio.h>
 /**
  * Positive_or_negative-tests to see if integer value is positive or negative
  * returns number sign
  */
 
-void postitive_or_negative(int);
+void positive_or_negative(int);
