029be0ce3778aacb6d95461a83eff1b0031020e4
Sithi <sithimbk@gmail.com>
2022-10-21 22:41:04 +0200

    is upper

---
+++  0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z' )
+	{
+		return (0);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return(10);
+	
+
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * @brief int _isupper - checks for uppercase letters
+ * 
+ * @param c 
+ * @return int - 
+ */
+
+int _isupper(int c);
+
+
+#endif  /* !defined(alx_low_level_ptogramming... 0x02-debugging) */
