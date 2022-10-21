55060ef64d44ac6855f97dda1789a6c7e62d4e56
Sithi <sithimbk@gmail.com>
2022-10-22 00:48:16 +0200

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+    print_line(0);
+    print_line(2);
+    print_line(10);
+    print_line(-4);
+    return (0);
+}
+
+/**
+ * print_line - print the character "_" n times to draw a line
+ * @n: number of characters to draw
+ */
+
+void print_line(int n)
+{
+	int count;
+	
+	for (count = 0; count <= n; count++)
+	{
+		line
+	}
+}
