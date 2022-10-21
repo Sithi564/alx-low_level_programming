0d9fab4dcaea1b48cc0dd2e1ce0c5990b2f18114
Sithi <sithimbk@gmail.com>
2022-10-21 23:54:17 +0200

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+	int i, p;
+
+	for (p = 0; p <= 10; p++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
