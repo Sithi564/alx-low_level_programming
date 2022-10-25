6c7128090fd3e9e6e8d96e3e892fb0a678d3573d
Sithi <sithimbk@gmail.com>
2022-10-25 04:18:06 +0200

    String length

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -10,6 +10,7 @@ void swap_int(int *a, int *b)
 {
 	int a_b;
 	int b_a;
+
 	a_b = *a;
 	b_a = *b;
 	*a = b_a;
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,18 @@
 
 /**
  * _strlen - calculates the length of a string.
- * 
+ *
  * Returns: string length.
  */
+
 int _strlen(char *s)
 {
-	
-    int len = 0;
-    while (*s != '\0') {
-        s++;
-        len++;
-    }
-    return len;
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
 }
