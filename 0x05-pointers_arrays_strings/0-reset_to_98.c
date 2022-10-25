37d09b0ccaf9156785b37ebf7ae7e37be200064b
Sithi <sithimbk@gmail.com>
2022-10-25 03:25:36 +0200

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void reset_to_98(int *n);
+
+/**
+ * reset_to_98 - points to variable and updates its value
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * reset_to_98 - takes a function to a poiinter and later updates it to 98.
+ * 
+ * Return: Always returns 0
+ */
+
+void reset_to_98(int *n);
+
+#endif
